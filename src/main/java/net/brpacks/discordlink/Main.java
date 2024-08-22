9ffec489307d22361963e91e36708988cea904cb
rok <rexblanegamer@gmail.com>
2024-08-22 03:42:36 -0300

    Logger

--- src/main/java/net/brpacks/discordlink/Main.java
+++ src/main/java/net/brpacks/discordlink/Main.java
@@ -3,13 +3,17 @@ package net.brpacks.discordlink;
 import lombok.Getter;
 import net.brpacks.discordlink.config.MainConfig;
 import net.brpacks.discordlink.jda.Bot;
+import org.apache.log4j.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
 public final class Main extends JavaPlugin {
 
+    public static final Logger LOGGER = Logger.getLogger("BrPacksDiscordLink");
+
     private static Main instance;
     private MainConfig mainConfig;
+
 
     @Override
     public void onEnable() {
