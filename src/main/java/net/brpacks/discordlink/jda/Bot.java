11f8231ce3dd6d823a1da315436dc86d3546af41
Sudano <joaohsudano@gmail.com>
2024-08-22 03:01:24 -0300

    registered sync command

--- src/main/java/net/brpacks/discordlink/jda/Bot.java
+++ src/main/java/net/brpacks/discordlink/jda/Bot.java
@@ -2,6 +2,7 @@ package net.brpacks.discordlink.jda;
 
 import lombok.Getter;
 import net.brpacks.discordlink.Main;
+import net.brpacks.discordlink.jda.commands.CommandVincular;
 import net.brpacks.discordlink.jda.listeners.ModalListeners;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -21,6 +22,7 @@ public class Bot {
 
         jda.upsertCommand("vincular", "Vincula sua conta do Minecraft com a do Discord").queue();
         jda.addEventListener(new ModalListeners());
+        jda.addEventListener(new CommandVincular());
 
         System.out.println("Bot iniciado com sucesso!");
 
